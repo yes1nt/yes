102c23fe635f203c610189a27b6474c78e8ff2b6
yes1nt <94116083+yes1nt@users.noreply.github.com>
2026-08-19 22:32:11 +0800

    Update Loader.lua

--- Deadrails/Loader.lua
+++ Deadrails/Loader.lua
@@ -6,3 +6,12 @@ Script.Cache.Config = {
 }
 Script.File.Save("FriedPotato/DeadRails/config.json", Script.Cache.Config)
 loadstring(game:HttpGet("https://raw.githubusercontent.com/yes1nt/yes/refs/heads/main/Deadrails/Main.lua"))()
+local ToggleButton = loadstring(game:HttpGet("https://raw.githubusercontent.com/yes1nt/yes/refs/heads/main/Script%20Tools/Toggler.lua"))()
+local PlayerGui = game:GetService("Players").LocalPlayer.PlayerGui
+local toggle = ToggleButton.MakeToggle("Friedpotato", function()
+	local gui = PlayerGui:FindFirstChild("PremiumUI")
+	if not gui then return end
+	local frame = gui:FindFirstChild("MainFrame")
+	if not frame then return end
+	frame.Visible = not frame.Visible
+end)
