941fbdcbcb2f83d428ee5822f8f6193333710879
yes1nt <94116083+yes1nt@users.noreply.github.com>
2026-09-12 14:47:01 +0800

    Update Main.lua to load new API endpoint

--- Deadrails/Main.lua
+++ Deadrails/Main.lua
@@ -1,3 +1,1 @@
-local MainAPI = loadstring(game:HttpGet("https://raw.githubusercontent.com/yes1nt/friedpotato/refs/heads/main/API/MAIN%20API.lua"))()
-local Main = MainAPI:LoadDeadrailsAPI()
-Main:Loader()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/yes1nt/friedpotato/refs/heads/main/API/Deadrails/Main.lua"))()
